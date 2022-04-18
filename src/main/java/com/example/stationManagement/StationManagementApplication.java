79c44d976305728b525af2bf8f16a2ff8ea6b67d
Siang <wsx13767@gmail.com>
2022-04-18 18:04:11 +0800

    初始設定

---
+++ src/main/java/com/example/stationManagement/StationManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.stationManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StationManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StationManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stationManagement/StationManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stationManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StationManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
