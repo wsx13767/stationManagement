34a5eaa96b9916df6941163e0ff044fe7442b6a0
Siang <wsx13767@gmail.com>
2022-04-19 20:17:02 +0800

    exceptionhandler新增exception抓取大部分錯誤資訊

--- src/main/java/com/example/stationManagement/exception/CustomExceptionHandler.java
+++ src/main/java/com/example/stationManagement/exception/CustomExceptionHandler.java
@@ -9,19 +9,40 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class CustomExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Error> exception(Exception ex) {
+        return ResponseEntity.internalServerError().body(new Error(ex.getMessage()));
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> methodArgumentNotValidException(MethodArgumentNotValidException ex) {
+    public ResponseEntity<Error> methodArgumentNotValidException(MethodArgumentNotValidException ex) {
         BindingResult result = ex.getBindingResult();
         StringBuilder sb = new StringBuilder();
         for (FieldError error : result.getFieldErrors()) {
             sb.append(error.getDefaultMessage()).append(";");
         }
-        return ResponseEntity.badRequest().body(sb.toString());
+        return ResponseEntity.badRequest().body(new Error(sb.toString()));
     }
 
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<String> runtimeException(RuntimeException ex) {
-        return ResponseEntity.badRequest().body(ex.getMessage());
+    public ResponseEntity<Error> runtimeException(RuntimeException ex) {
+        return ResponseEntity.badRequest().body(new Error(ex.getMessage()));
+    }
+
+
+    class Error {
+        private String error;
+
+        public Error(String error) {
+            this.error = error;
+        }
+
+        public String getError() {
+            return error;
+        }
+
+        public void setError(String error) {
+            this.error = error;
+        }
     }
 }
